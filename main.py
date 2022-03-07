5382641be9ed4f0fe0d71b6ea538a23c823f364d
Matt / <77175926+Epic-Tank@users.noreply.github.com>
2022-03-07 02:16:51 +0000

    i did the e

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","bossbabymini.mp4",.3,3,20)
+    encoder.encode("RatatouilleEnding","bossbabymini.mp4",.3,10,15)
 
 if __name__== "__main__":
     main()
